5133f73975f4463dcee11594091b8759da06121a
alonsitman <132371077+alonsitman@users.noreply.github.com>
2024-02-07 15:31:34 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,145 @@
+'use strict'
+
+var gBoard
+
+var gLevel = {
+    SIZE: 4,
+    MOSQUITO: 2
+}
+
+var gGame = {
+    isOn: false,
+    shownCount: 0,
+    markedCount: 0,
+    secsPassed: 0
+}
+
+const MOSQUITO_IMG = '<img src="img/mosquito.png">'
+
+
+function onInit() {
+    gGame.isOn = true
+    gBoard = buildBoard()
+    renderBoard(gBoard)
+    
+    // for (let i = 0; i < gBoard.length; i++) {
+    //     for (let j = 0; j < gBoard[0].length; j++) {
+    //         renderCell({i: i, j: j}, setMosquitoesNegsCount(gBoard, i, j))
+    //     }
+    // }
+}
+
+function buildBoard() {
+    const rows = gLevel.SIZE
+    const cols = gLevel.SIZE
+    const board = createMat(rows, cols)
+    
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+			board[i][j] = {mosquitoesAroundCount: 0, isShown: false, isMosquito: false, isMarked: false}
+        }
+    }
+
+    board[0][2].isMosquito = true
+    board[1][1].isMosquito = true
+    // randSpreadMosquitoes(board)
+    
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+			const numMosqNegs = setMosquitoesNegsCount(board, i, j)
+        }
+    }
+
+    // console.log(board)
+    return board
+}
+    
+function setMosquitoesNegsCount(board, rowIdx, colIdx) {
+    var count = 0
+
+    for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+        if (i < 0 || i >= board.length) continue
+        for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+            if (i === rowIdx && j === colIdx) continue
+            if (j < 0 || j >= board[0].length) continue
+            var currCell = board[i][j]
+            if (currCell.isMosquito === true) count++
+        }
+    }
+
+    board[rowIdx][colIdx].mosquitoesAroundCount = count
+    // console.log('neighboring mosquitoes count: ' + rowIdx + ' ' + colIdx + ' - ' + count)
+    return count
+}
+
+function renderBoard(board) {
+    const elBoard = document.querySelector('.board')
+    
+    var strHTML = ''
+    for (let i = 0; i < board.length; i++) {
+        strHTML += '<tr>\n'
+        
+        for (let j = 0; j < board[0].length; j++) {
+            const currCell = board[i][j]
+            var cellClass = getClassName({i: i, j: j})
+
+            if (currCell.isMosquito === true) cellClass += ' mosquito'
+
+            strHTML += '\t<td class="cell ' + cellClass + '">\n'
+            if (currCell.isMosquito === true) strHTML += MOSQUITO_IMG
+
+            strHTML += '\t</td>\n'
+        }
+
+        strHTML += '</tr>\n'
+    }
+
+    console.log('strHTML:\n', strHTML)
+    elBoard.innerHTML = strHTML
+}
+
+function renderCell(location, value) {
+    const cellSelector = '.' + getClassName(location)
+    // console.log('cellSelector:', cellSelector)
+	const elCell = document.querySelector(cellSelector)
+	elCell.innerHTML = value
+    // console.log('elCell:', elCell)
+}
+
+function onCellClicked(elCell, i, j) {
+
+}
+
+function onCellMarked(elCell) {
+
+}
+
+function checkGameOver() {
+
+}
+
+function expandShown(board, elCell, i, j) {
+
+}
+
+function randSpreadMosquitoes(board) {
+    const rows = board.length
+    const cols = board[0].length
+    
+    var numMosquitoes = 0
+    while (numMosquitoes !== gLevel.MOSQUITO) {
+        for (let i = 0; i < gLevel.MOSQUITO; i++) {
+            var randI = getRandomInt(0, rows - 1)
+            var randJ = getRandomInt(0, cols - 1)
+
+            if (board[randI][randJ].isMosquito === false) {
+                board[randI][randJ].isMosquito = true
+                numMosquitoes++
+            }
+        }
+
+        if (numMosquitoes !== gLevel.MOSQUITO) numMosquitoes = 0
+    }
+}
+
---
+++ js/utils.js
@@ -0,0 +1,45 @@
+'use strict'
+
+function createMat(ROWS, COLS) {
+    const mat = []
+    for (var i = 0; i < ROWS; i++) {
+        const row = []
+        for (var j = 0; j < COLS; j++) {
+            row.push('')
+        }
+        mat.push(row)
+    }
+    return mat
+}
+
+function getRandomInt(min, max) { 
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function getClassName(location) {
+	const cellClass = 'cell-' + location.i + '-' + location.j
+	return cellClass
+}
+
+// for these to work need css class .hide or different solution
+// function showElement(selector) {
+//     const element = document.querySelector(selector)
+//     element.classList.remove('hide')
+// }
+
+// function hideElement(selector) {
+//     const element = document.querySelector(selector)
+//     element.classList.add('hide')
+// }
